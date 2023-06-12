b2d04ac23d7ef2fc9e4119bfa06a37678802c32f
Sean Peters <sean@snptrs.dev>
2023-06-12 11:49:33 +0100

    Completes module 5

---
+++ bites/5-ex1-signs.js
@@ -0,0 +1,11 @@
+const getNumberSign = (n) => {
+  if (n === 0) {
+    return "zero";
+  } else if (n > 0) {
+    return "positive";
+  } else {
+    return "negative";
+  }
+};
+
+module.exports = getNumberSign;
---
+++ bites/5-ex2-debug.js
@@ -0,0 +1,8 @@
+const isValidLength = (phoneNumber) => {
+  const validLength = 11;
+  if (phoneNumber.length == validLength) {
+    return true;
+  } else {
+    return false;
+  }
+};
