481b08d1eec4f09ff63215e1803bdf10db4a6989
Sean Peters <sean@snptrs.dev>
2023-06-14 17:09:02 +0100

    Completes 2.5

---
+++ challenges/5-pokedex/api.js
@@ -0,0 +1,31 @@
+class Pokedex {
+  constructor() {
+    this.pokedex = [];
+  }
+  fetchPokemon = (name) => {
+    return fetch("https://pokeapi.co/api/v2/pokemon/" + name)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const types = data.types.map((item) => item.type.name);
+        return {
+          name: data.name,
+          id: data.id,
+          height: data.height,
+          weight: data.weight,
+          types: types,
+        };
+      });
+  };
+
+  catch(name) {
+    this.fetchPokemon(name).then((pokemon) => this.pokedex.push(pokemon));
+  }
+
+  all() {
+    return this.pokedex;
+  }
+}
+
+module.exports = Pokedex;
---
+++ challenges/5-pokedex/api.test.js
@@ -0,0 +1,25 @@
+const Pokedex = require("./api.js");
+
+describe("Pokedex", () => {
+  it("Can fetch the name for Ditto", (done) => {
+    const pokedex = new Pokedex();
+    pokedex.fetchPokemon("ditto").then((pokemon) => {
+      expect(pokemon.name).toEqual("ditto");
+      done();
+    });
+  });
+
+  it("Can fetch the id for Ditto", () => {
+    const pokedex = new Pokedex();
+    pokedex.fetchPokemon("ditto").then((pokemon) => {
+      expect(pokemon.id).toEqual(132);
+    });
+  });
+
+  it("Can fetch the height for Ditto", () => {
+    const pokedex = new Pokedex();
+    pokedex.fetchPokemon("ditto").then((pokemon) => {
+      expect(pokemon.weight).toEqual(40);
+    });
+  });
+});
