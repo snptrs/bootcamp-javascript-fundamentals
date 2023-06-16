7b909f91fd37ed16c1360c5f84a8e523b570a979
Sean Peters <sean@snptrs.dev>
2023-06-16 08:27:55 +0100

    Adds test for .compareWith

--- challenges/6-weather/weather.test.js
+++ challenges/6-weather/weather.test.js
@@ -1,11 +1,10 @@
 const Weather = require("./weather.js");
 
 describe("Weather", () => {
-  it("loads and returns the weather for a city ", () => {
-    const mockClient = {
-      fetchWeatherData: jest.fn(),
-    };
-
+  const mockClient = {
+    fetchWeatherData: jest.fn(),
+  };
+  it("loads and returns the weather for a city", () => {
     mockClient.fetchWeatherData.mockResolvedValueOnce({
       name: "London",
       main: {
@@ -17,6 +16,29 @@ describe("Weather", () => {
     weather.load("London").then(() => {
       expect(weather.getWeatherData().name).toEqual("London");
       expect(weather.getWeatherData().main.temp).toEqual(25);
+    });
+  });
+
+  it("compares the weather for two cities", () => {
+    mockClient.fetchWeatherData.mockResolvedValueOnce({
+      name: "London",
+      main: {
+        temp: 25,
+      },
+    });
+
+    const weather = new Weather(mockClient);
+    weather.load("London");
+
+    mockClient.fetchWeatherData.mockResolvedValueOnce({
+      name: "Dublin",
+      main: {
+        temp: 25,
+      },
+    });
+
+    weather.compareWith("Dublin").then((response) => {
+      expect(response).toEqual("London is warmer than Dublin");
     });
   });
 });
