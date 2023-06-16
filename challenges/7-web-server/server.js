159c1adfb96abcfe284426c6c1419b03be87af39
Sean Peters <sean@snptrs.dev>
2023-06-16 13:52:36 +0100

    Completes exercise 7

--- challenges/7-web-server/server.js
+++ challenges/7-web-server/server.js
@@ -2,8 +2,24 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+let counter = 0;
+
 app.get("/", (req, res) => {
   res.send("Hello, world!");
+});
+
+app.post("/counter", (req, res) => {
+  counter++;
+  res.send(`${counter}`);
+});
+
+app.get("/counter", (req, res) => {
+  res.send(`${counter}`);
+});
+
+app.delete("/counter", (req, res) => {
+  counter = 0;
+  res.send(`${counter}`);
 });
 
 console.log(`Server listening on localhost:${port}`);
